a26452f0447991c428dd1834075b6ef3aace6e9f
Martin <109266475+martinjv9@users.noreply.github.com>
2024-11-13 16:50:10 -0800

    Initializing Project

---
+++ src/main/java/org/thewrooms/openbench/OpenBenchApplication.java
@@ -0,0 +1,13 @@
+package org.thewrooms.openbench;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenBenchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenBenchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/thewrooms/openbench/OpenBenchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.thewrooms.openbench;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenBenchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
