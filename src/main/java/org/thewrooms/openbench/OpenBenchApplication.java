630c8b3cc0eadb63e19c136f13e46d5ac8009fd2
Martin <109266475+martinjv9@users.noreply.github.com>
2024-11-13 17:28:44 -0800

    BLURURUR

--- src/main/java/org/thewrooms/openbench/OpenBenchApplication.java
+++ src/main/java/org/thewrooms/openbench/OpenBenchApplication.java
@@ -3,12 +3,14 @@ package org.thewrooms.openbench;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class OpenBenchApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OpenBenchApplication.class, args);
         System.out.println("OpenBench application started");
+        System.out.println("Hello World!");
     }
 
 }
