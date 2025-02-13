bc1b80b6057907cdaf80efb16a47c238764bb98e
Amy Truong <amyktruongdev@gmail.com>
2025-02-12 17:24:50 -0800

    Fixing navbar

--- client/src/pages/Add.jsx
+++ client/src/pages/Add.jsx
@@ -7,39 +7,38 @@ import Time from '../Time';
 const Add = () => {
   return (
     <div>
+      <Nav /> {/* Navbar will stay fixed on the left */}
+      <div className="content">
         <h1 className="header">OpenBench</h1>
         <Time />
-        <Nav />
-    <div className="logOut">
-    <button><Link to="/update">Logout</Link></button>
-    </div>
-      <div className="box-container">
-        <div className="row">
-          <div className="box">
-            <h2>Gym Overall</h2>
-            <h2 className="amount">15</h2>
-            <div className="word-spacing">Last updated 4:00pm</div>
+        <div className="box-container">
+          <div className="row">
+            <div className="box">
+              <h2>Gym Overall</h2>
+              <h2 className="amount">15</h2>
+              <div className="word-spacing">Last updated 4:00pm</div>
+            </div>
+            <div className="box">
+              <h2>Treadmill</h2>
+              <h2 className="amount">15</h2>
+              <div className="word-spacing">Last updated 4:00pm</div>
+            </div>
           </div>
-          <div className="box">
-            <h2>Treadmill</h2>
-            <h2 className="amount">15</h2>
-            <div className="word-spacing">Last updated 4:00pm</div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="box">
-            <h2>Bench</h2>
-            <h2 className="amount">15</h2>
-            <div className="word-spacing">Last updated 4:00pm</div>
-          </div>
-          <div className="box">
-            <h2>Weights</h2>
-            <h2 className="amount">15</h2>
-            <div className="word-spacing">Last updated 4:00pm</div>
+          <div className="row">
+            <div className="box">
+              <h2>Bench</h2>
+              <h2 className="amount">15</h2>
+              <div className="word-spacing">Last updated 4:00pm</div>
+            </div>
+            <div className="box">
+              <h2>Weights</h2>
+              <h2 className="amount">15</h2>
+              <div className="word-spacing">Last updated 4:00pm</div>
+            </div>
           </div>
         </div>
+        <Footer />
       </div>
-      <Footer />
     </div>
   );
 };
