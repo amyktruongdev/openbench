0cafb8dca70df8c57b650fcb72905de6c0137780
jeh-loh <angelochristian.panopio.568@my.csun.edu>
2025-02-23 19:49:39 -0800

    For the MQTT test.

---
+++ MQTT_test/MQTT_test.ino
@@ -0,0 +1,78 @@
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+const char* ssid = "insert_wifi_name";
+const char* password = "insert_password";
+const char* mqtt_server = "insert_dns";
+const int mqtt_port = 1883; // Default MQTT port
+const char* mqtt_topic = "insert_topic";
+const char* mqtt_client_id = "esp32_client";
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+void setup_wifi() {
+  delay(10);
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (int i = 0; i < length; i++) {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    
+    if (client.connect(mqtt_client_id)) {
+      Serial.println("connected");
+      client.subscribe(mqtt_topic);
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+}
+
+void loop() {
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+  // Example message to publish
+  char message[50];
+  sprintf(message, "PLS WORK PLS");
+  client.publish(mqtt_topic, message);
+  
+  delay(5000); // Adjust delay as needed
+}
